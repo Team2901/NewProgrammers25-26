669e2542b88074f1b246b12833e60d804ea6751a
condecorsinog29209 <condecorsinog29209@student.cghsnc.org>
2025-09-09 17:00:27 -0400

    I started coding

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TaylorinBudgit.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "TaylorinBudgit")
+public class TaylorinBudgit extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
