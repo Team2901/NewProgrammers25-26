719443b5604918166b6268fc4e2265ffc98b23b0
selzerk29587 <Selzerk29587@student.cghsnc.org>
2025-09-20 12:59:06 -0400

    New trig for the gamepad and imu value was added.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MecanumDrive.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "MecanumDrive-Kyle")
 public class MecanumDrive extends OpMode {
@@ -13,6 +17,8 @@ public class MecanumDrive extends OpMode {
     DcMotor backLeft;
     DcMotor backRight;
 
+    IMU imu;
+
 
 
     @Override
@@ -20,31 +26,62 @@ public class MecanumDrive extends OpMode {
         telemetry.addLine("MecanumDive");
 
         frontLeft = hardwareMap.dcMotor.get("frontLeft");
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRight = hardwareMap.dcMotor.get("frontRight");
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft = hardwareMap.dcMotor.get("backLeft");
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight = hardwareMap.dcMotor.get("backRight");
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        // This needs to be changed to match the orientation on your robot
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection =
+                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection =
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
+
+        RevHubOrientationOnRobot orientationOnRobot = new
+                RevHubOrientationOnRobot(logoDirection, usbDirection);
+        boolean success = imu.initialize(new IMU.Parameters(orientationOnRobot));
+        telemetry.addData("imu init", success);
     }
 
     @Override
     public void loop() {
+        telemetry.addData("robot angle", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+        telemetry.addData("x_value", gamepad1.left_stick_x);
+        telemetry.addData("y_value", gamepad1.left_stick_y);
+
         double y = gamepad1.left_stick_y;
         double x = gamepad1.left_stick_x;
         double rx = gamepad1.right_stick_x;
+        double control_angle = Math.atan2(y, x) - Math.toRadians(90);
+        double magnatude = Math.hypot(y, x);
+        double forward = Math.cos(control_angle) * magnatude;
+        double right = Math.sin(control_angle) * magnatude;
+        move(right, forward, rx);
+
+
+    }
+    public void move(double x, double y, double rx){
 
         frontLeft.setPower(y + x + rx);
         backLeft.setPower(y - x + rx);
         frontRight.setPower(y - x - rx);
         backRight.setPower(y + x - rx);
+
 
     }
 }
