8b230cfb607e7fdc0a6f58bead542bcc98170f17
shawc29591 <shawc29591@student.cghsnc.org>
2025-10-04 12:59:13 -0400

    Made it go side to side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Clara.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Clara.java
@@ -15,7 +15,7 @@ import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
-@TeleOp(name = "Clara Teleop V5")
+@TeleOp(name = "Clara Teleop V6")
 public class Clara extends OpMode {
 
    String name;
